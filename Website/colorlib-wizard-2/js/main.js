b8e79e1d5b29b7a26ce75ac560b4e4ad7de7ca3c
Danish Mir <76698292+mirdanish6594@users.noreply.github.com>
2023-08-03 01:09:16 +0530

    search-results.html and more
    added responsive cards to search results, added request form and also made some other minor changes.

---
+++ Website/colorlib-wizard-2/js/main.js
@@ -0,0 +1,63 @@
+$(function(){
+	$("#wizard").steps({
+        headerTag: "h2",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        transitionEffectSpeed: 300,
+        labels: {
+            finish: "Submit",
+            next: "Forward",
+            previous: "Backward"
+        },
+        onFinished: function (event) {
+
+            var formData = $("#wizard").serialize();
+            formData += '&category=' + $("#category").text();
+            var productID = Date.now() + Math.floor(Math.random() * 1000);
+            formData += '&productID='+ productID;
+            console.log(formData);
+            // Send data using AJAX
+            $.ajax({
+                type: "POST",
+                url: "http://127.0.0.1/push.php", // Replace with the actual path to your PHP script
+                data: formData,
+                success: function(response) {
+                  // Process the response from PHP (if needed)
+                  console.log(response);
+                },
+                error: function(xhr, status, error) {
+                  // Handle errors (if any)
+                  console.error(error);
+                }
+              });
+        }
+    });
+    $('.wizard > .steps li a').click(function(){
+    	$(this).parent().addClass('checked');
+		$(this).parent().prevAll().addClass('checked');
+		$(this).parent().nextAll().removeClass('checked');
+    });
+    // Custome Jquery Step Button
+    $('.forward').click(function(){
+    	$("#wizard").steps('next');
+    })
+    $('.backward').click(function(){
+        $("#wizard").steps('previous');
+    })
+    // Select Dropdown
+    $('html').click(function() {
+        $('.select .dropdown').hide(); 
+    });
+    $('.select').click(function(event){
+        event.stopPropagation();
+    });
+    $('.select .select-control').click(function(){
+        $(this).parent().next().toggle();
+    })    
+    $('.select .dropdown li').click(function(){
+        $(this).parent().toggle();
+        var text = $(this).attr('rel');
+        $(this).parent().prev().find('div').text(text);
+    })
+})
