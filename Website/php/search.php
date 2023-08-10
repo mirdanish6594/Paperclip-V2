74a27f05f6f5fb84e18dd65fbcc4a25d112a8655
Ayman Makroo <ajm8982@gmail.com>
2023-08-10 10:45:15 +0530

    🔗 Updated Search.php

--- Website/php/search.php
+++ Website/php/search.php
@@ -2,16 +2,29 @@
     include('databaseConfigration.php');
 
     mysqli_select_db($mysqli, $dbName);
+    if ($_GET['search'] != "" && $_GET['category'] != ""){
 
-    $category = $_GET['category'];
-    
-    if (isset($_GET['search'])){
         $searchTerm = $_GET['search'];
-    } else {
-        $searchTerm = '';
+        $category = $_GET['category'];
+        $query = "SELECT * FROM products WHERE category = $category AND title LIKE '%$searchTerm%' ORDER by title ASC";
+
+    }elseif($_GET['search'] != "") {
+
+        $searchTerm = $_GET['search'];
+        $query = "SELECT * FROM products WHERE title LIKE '%$searchTerm%' ORDER by title ASC";
+
+    }elseif($_GET['category'] != ""){
+
+        $category = $_GET['category'];
+        $query = "SELECT * FROM products WHERE category = $category ORDER by title ASC";
+        
+    }else{
+
+        $query = "SELECT * FROM products ORDER by title ASC";
+        
     }
 
-    $query = "SELECT * FROM products WHERE category = $category AND title LIKE '%$searchTerm%' ORDER by title ASC";
+   
 
     $result = mysqli_fetch_all(mysqli_query($mysqli, $query), MYSQLI_ASSOC);
 
